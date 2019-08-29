12fe13690ebe49ce1608b8e2ac0fa23abc686024
Mudassar Ali <mudassar.ali@jintech.com>
2019-08-29 15:14:02 +0500

    individual images display

--- phone-detail/phone-detail.component.js
+++ phone-detail/phone-detail.component.js
@@ -1,14 +1,18 @@
 angular.
 	module('phoneDetail').
 	component('phoneDetail',{
-		 templateUrl: 'phone-detail/phone-detail.template.html',
+		templateUrl: 'phone-detail/phone-detail.template.html',
 		controller:['$http','$routeParams',
 			function PhoneDetailController($http,$routeParams){
-				console.log("Hmmm");
+			
 				var self = this;
+				self.setImage = function setImage(imageUrl){
+
+					self.mainImageUrl = imageUrl;
+				}
 				$http.get('phones/'+$routeParams.phoneId+'.json').then(function(response){
 					self.phone = response.data;
-					console.log(response.data);
+					self.setImage(self.phone.images[0]);
 				});
 				this.phoneId = $routeParams.phoneId;
 		}
